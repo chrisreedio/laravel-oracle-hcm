f301de1e14029b38c56a79412e24d3c63805c2b5
Chris Reed <chris@reedtech.us>
2024-09-30 13:52:17 -0500

    ✨ feat: added `updates` method to `EmployeeFeed` for employee updates
    Introduced a new `updates` method to fetch employee updates. This enhances the functionality of the `EmployeeFeed` class.

--- src/Resources/Feeds/EmployeeFeed.php
+++ src/Resources/Feeds/EmployeeFeed.php
@@ -13,10 +13,26 @@ readonly class EmployeeFeed
         //
     }
 
+    /**
+     * Generic method to get a feed collection
+     * @param string $collection
+     * @return Response
+     * @throws \Saloon\Exceptions\Request\FatalRequestException
+     * @throws \Saloon\Exceptions\Request\RequestException
+     */
     public function get(string $collection): Response
     {
         $request = new GetFeedCollection('employee', $collection);
 
         return $this->connector->send($request);
     }
+
+    public function updates(): Response
+    {
+        $request = new GetFeedCollection('employee', 'empupdate');
+
+        return $this->connector->send($request);
+    }
+
+
 }
